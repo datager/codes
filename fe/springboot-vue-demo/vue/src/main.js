e25513406b27a059fbe157bda74dd6e4f86b082d
datager <daska110@126.com>
2021-09-12 15:47:20 +0800

    feat: http file upload, and news.vue

---
+++ fe/springboot-vue-demo/vue/src/main.js
@@ -0,0 +1,20 @@
+import "default-passive-events";
+
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+import "@/assets/css/global.css";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus, {
+    locale: zhCn,
+    size: "small",
+  })
+  .mount("#app");
---
+++ fe/springboot-vue-demo/vue/src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Layout from "../Layout/Layout.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Layout",
+    component: Layout,
+    redirect: "/user",
+    children: [
+      {
+        path: "user",
+        name: "User",
+        component: () => import("@/views/User"),
+      },
+      {
+        path: "book",
+        name: "Book",
+        component: () => import("@/views/Book"),
+      },
+      {
+        path: "news",
+        name: "News",
+        component: () => import("@/views/News"),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: import("@/views/Login"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: import("@/views/Register"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
